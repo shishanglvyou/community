58eff6ec7a83ab9b975f6e8d0e58c0f2d6555cbd
shishanglvyou <693365957@qq.com>
2019-10-08 21:55:32 +0800

    .

--- src/main/java/com/rainotes/community/controller/IndexController.java
+++ src/main/java/com/rainotes/community/controller/IndexController.java
@@ -1,13 +1,17 @@
 package com.rainotes.community.controller;
 
+import com.rainotes.community.dto.QuestionDTO;
 import com.rainotes.community.mapper.UserMapper;
 import com.rainotes.community.model.User;
+import com.rainotes.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -15,9 +19,16 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(
+            HttpServletRequest request,
+            Model model
+                        ) {
         Cookie[] cookies=request.getCookies();
+        if (cookies != null && cookies.length != 0)
         for (Cookie cookie : cookies) {
             if(cookie.getName().equals("token")){
                 String token=cookie.getValue();
@@ -28,6 +39,9 @@ public class IndexController {
                 break;
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
---
+++ src/main/java/com/rainotes/community/service/QuestionService.java
@@ -0,0 +1,33 @@
+package com.rainotes.community.service;
+
+import com.rainotes.community.dto.QuestionDTO;
+import com.rainotes.community.mapper.QuestionMapper;
+import com.rainotes.community.mapper.UserMapper;
+import com.rainotes.community.model.Question;
+import com.rainotes.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
+    public List<QuestionDTO> list() {
+        List<Question> questions = questionMapper.list();
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions) {
+           User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        return questionDTOList;
+    }
+}
